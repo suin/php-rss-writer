b5817dbfa0ec2238221291710e1cb79bf184bc9a
Craig Meyer <craig@meyerscitech.com>
2017-07-24 12:49:17 -0400

    Extend Feed to support CSS style sheet specification

--- src/Suin/RSSWriter/Feed.php
+++ src/Suin/RSSWriter/Feed.php
@@ -13,6 +13,24 @@ class Feed implements FeedInterface
     /** @var ChannelInterface[] */
     protected $channels = [];
 
+    protected $feed_styles = [];
+
+    /**
+     * Add Style sheet
+     * @param string  $style_url
+     * @param string  $media  (defaults to 'screen')
+     * @return $this
+     */
+    public function addStyle($style_url, $media = 'screen'){
+      if( !empty($style_url) ){
+	debug("Adding Style: $style_url");
+	$this->feed_styles[] = [ $style_url, $media ];
+      }
+
+      return $this;
+    }
+
+      
     /**
      * Add channel
      * @param ChannelInterface $channel
@@ -28,17 +46,33 @@ class Feed implements FeedInterface
      * Render XML
      * @return string
      */
-    public function render()
-    {
-        $xml = new SimpleXMLElement('<?xml version="1.0" encoding="UTF-8" ?><rss version="2.0" xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:atom="http://www.w3.org/2005/Atom" />', LIBXML_NOERROR | LIBXML_ERR_NONE | LIBXML_ERR_FATAL);
+    public function render(){
 
-        foreach ($this->channels as $channel) {
+      $xml = new SimpleXMLElement("<?xml version=\"1.0\" encoding=\"UTF-8\" ?><rss version=\"2.0\" xmlns:content=\"http://purl.org/rss/1.0/modules/content/\" xmlns:atom=\"http://www.w3.org/2005/Atom\" />",
+				  LIBXML_NOERROR | LIBXML_ERR_NONE | LIBXML_ERR_FATAL);
+
+
+      foreach ($this->channels as $channel) {
             $toDom = dom_import_simplexml($xml);
+	    debug("toDom: " . print_r($toDom, true) );
             $fromDom = dom_import_simplexml($channel->asXML());
             $toDom->appendChild($toDom->ownerDocument->importNode($fromDom, true));
         }
 
         $dom = new DOMDocument('1.0', 'UTF-8');
+
+	if( count($this->feed_styles) > 0 ){
+	  foreach( $this->feed_styles as $info ){
+	    $url = $info[0];
+	    $media = $info[1];
+	    debug("Feed_Style: " . $url);
+	    $xslt = $dom->createProcessingInstruction('xml-stylesheet', "type=\"text/css\" media=\"{$media}\" href=\"{$url}\" ");
+	    $dom->appendChild($xslt);
+	  }
+	}
+
+	debug("XML: " . print_r(dom_import_simplexml($xml), true) );
+
         $dom->appendChild($dom->importNode(dom_import_simplexml($xml), true));
         $dom->formatOutput = true;
         return $dom->saveXML();
